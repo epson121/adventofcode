ed24d62cb8c51f5155c585d054ed578fa3d33fc2
epson121 <traxdater12@gmail.com>
2016-12-18 13:54:11 +0100

    clean 18

--- 2016/18.rb
+++ 2016/18.rb
@@ -3,7 +3,6 @@ file = File.new("18-test.txt", "r")
 file = File.new("18.txt", "r")
 row = "." + file.gets + "."
 
-# rows = [row]
 i = 0
 safes = row.count(".") - 2
 cr = row
@@ -13,7 +12,6 @@ invalid = ["^^.", "^..", ".^^", "..^"]
     1.upto(row.length-2) do |idx|
         case 
             when !invalid.include?(cr[idx-1..idx+1])
-            # when cr[idx-1..idx+1].match(/(\^\^\.|\^\.\.|\.\^\^|\.\.\^)/).nil?
                 new_row << "."
             else
                 new_row << "^"
@@ -23,13 +21,7 @@ invalid = ["^^.", "^..", ".^^", "..^"]
     safes += new_row.count(".") - 1
     new_row << "."
     cr = new_row.join("")
-    # rows << new_row.join("")
     i += 1
-    # if i % 10000 == 0
-    #     p i
-    # end
 
 end
-# rows.each {|r| p r[1..r.length-2]}
-# p rows.length
 p safes
